0a4ab06e8c091b8f9d69f1c604ef46faa9db54ab
karthikn1997 <karthikn951@gmail.com>
2024-05-24 13:52:43 +0530

    ok

--- src/Kyc-form/Step6.jsx
+++ src/Kyc-form/Step6.jsx
@@ -5,9 +5,9 @@ const Step6 = () => {
   return (
     <div>
       <h1 className='text-2xl mb-3 font-semibold'>Please Check Your Email Account For Email Verification</h1>
-      <p className='text-center flex flex-col'>
-        <span className='text-lg'>Go to Login Page, After Email Verification</span>
-        <Link to="/userLogin" className="font-semibold hover:underline">
+      <p className='text-center flex flex-col gap-1'>
+        <span className='text-md'>Go to Login Page, After Email Verification</span>
+        <Link to="/userLogin" className="font-semibold hover:underline border-2 py-1 bg-gray-200">
           Click Here
         </Link>
       </p>
