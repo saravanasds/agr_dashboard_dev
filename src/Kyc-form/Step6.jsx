782981088c688b181ed99634f8f08d2fc86ad7ec
karthikn1997 <karthikn951@gmail.com>
2024-05-24 13:41:22 +0530

    commit

--- src/Kyc-form/Step6.jsx
+++ src/Kyc-form/Step6.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Step6 = () => {
   return (
     <div>
-        <h1>Please Check Your Email Account For Email Verification</h1>
+      <h1 className='text-2xl mb-3 font-semibold'>Please Check Your Email Account For Email Verification</h1>
+      <p className='text-center flex flex-col'>
+        <span className='text-lg'>Go to Login Page, After Email Verification</span>
+        <Link to="/userLogin" className="font-semibold hover:underline">
+          Click Here
+        </Link>
+      </p>
     </div>
   )
 }
